e534bb1c1527fb03321ad4f8e7d93fd17fd9094f
Akarshb23 <akarshb23@gmail.com>
2026-03-17 09:25:34 +0530

    dom finished

--- dom.js
+++ dom.js
@@ -17,7 +17,7 @@ result1.title = "myname" ;
 //
 const result2 = document.getElementsByClassName("p1");
 console.log(result2);
-result2.innerHTML = "myself";
+result2[0].innerHTML = "myself";
 result2[1].innerHTML = "AKARSH";
 
 //
@@ -36,12 +36,33 @@ result4.style.backgroundColor = "green";
 result4.style.padding = "10px";
 
 //
-// query selector all 
+// query selector all (this is for all selectors)
 //
 const result5 = document.querySelectorAll("ul li"); // any id , class , or tag
-    for(x in result5){
-        result5[x].style.backgroundColor = "green";
-        result5[x].style.margin = "10px";
-        result5[x].style.color = "blue";
+for(x in result5){
+    result5[x].style.backgroundColor = "green";
+    result5[x].style.margin = "10px";
+    result5[x].style.padding = "10px";
+    result5[x].style.color = "white";
+}
 
-    }
+//
+// getAttribute(attr)
+//
+const element = document.getElementById("myH1");
+let text = element.getAttribute("class");
+document.getElementById("demo").innerHTML = text ;
+
+//
+// setAttribute(attr,value)
+//
+function myFunction(){
+    document.getElementById("myH1").setAttribute("class","democlass");
+    
+    console.log(document.getElementById("myH1").getAttribute("class"));
+}
+
+// 
+// DOM append method
+//
+const heading = document.createElement("h2");
